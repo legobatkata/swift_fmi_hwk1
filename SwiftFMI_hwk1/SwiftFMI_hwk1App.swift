799317457a47135c81849592772d3b2102522921
legobatkata <legobatkata@abv.bg>
2024-11-10 18:21:55 +0200

    Initial Commit

---
+++ SwiftFMI_hwk1/SwiftFMI_hwk1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFMI_hwk1App.swift
+//  SwiftFMI_hwk1
+//
+//  Created by Ivaylo Atanasov on 10.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFMI_hwk1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
