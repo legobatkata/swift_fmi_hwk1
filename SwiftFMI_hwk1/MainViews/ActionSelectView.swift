0f292c31e63a8a6c4080c8a3387cd9801b976483
legobatkata <legobatkata@abv.bg>
2024-11-18 01:39:17 +0200

    added the homework code

---
+++ SwiftFMI_hwk1/MainViews/ActionSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  ActionSelectView.swift
+//  SwiftFMI_hwk1
+//
+//  Created by Ivaylo Atanasov on 13.11.24.
+//
+
+import SwiftUI
+
+struct ActionSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActionSelectView()
+}
---
+++ SwiftFMI_hwk1/MainViews/ChapterSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  ChapterSelectView.swift
+//  SwiftFMI_hwk1
+//
+//  Created by Ivaylo Atanasov on 13.11.24.
+//
+
+import SwiftUI
+
+struct ChapterSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChapterSelectView()
+}
